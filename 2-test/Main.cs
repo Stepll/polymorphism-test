edbadb8264efb44c3b30ac4a5673d385434afcc9
StepanKobrii18321832 <esestepan2000@gmail.com>
2020-06-10 12:47:11 +0300

    add-class-type

--- 2-test/Main.cs
+++ 2-test/Main.cs
@@ -12,15 +12,105 @@ namespace Shop
 
         public User(string Name, int Age)
         {
-            //constructor
+            this.Name = Name;
+            this.Age = Age;
         }
         public User(string Name, int Age, string Phone_number)
         {
-            //constructor
+            this.Name = Name;
+            this.Age = Age;
+            this.Phone_number = Phone_number;
         }
         public User(string Name, int Age, string Phone_number, double Balance)
         {
-            //constructor
+            this.Name = Name;
+            this.Age = Age;
+            this.Phone_number = Phone_number;
+            this.Balance = Balance;
+        }
+
+        public void ReduceBalance (double price) 
+        {
+            Balance -= price;
+            Spend += price;
+        }
+    }
+
+    class Product 
+    {
+        public string Name {get; set;}
+        public double Price {get; set;}
+        public string Manufacturer {get; set;}
+        public double GetDiscountPrice(User user)
+        {
+            if (user.Spend < 1000)
+            {
+                return Price;
+            }
+
+            if (user.Spend < 5000)
+            {
+                return Price * 0.93;
+            }
+
+            return Price * 0.85;
+        }
+
+    }
+
+    class Cloth : Product
+    {
+        public string Material { get; private set; }
+
+        public Cloth(string name, int price, string manufacturer, string material)
+        {
+            Name = name;
+            Manufacturer = manufacturer;
+            Price = price;
+            Material = material;
+        }
+    }
+
+    class Fruits : Product
+    {
+        public string Variety { get; private set; }
+
+        public Fruits(string name, int price, string manufacturer, string variety)
+        {
+            Name = name;
+            Manufacturer = manufacturer;
+            Price = price;
+            Variety = variety;
+        }
+    }
+
+    class Phone : Product
+    {
+        public string Size { get; private set; }
+        public string OperatingSystem { get; private set; }
+        public string CameraResolution { get; private set; } 
+
+        public Phone(string name, int price, string manufacturer, string size, string operatingSystem, string cameraResolution)
+        {
+            Name = name;
+            Manufacturer = manufacturer;
+            Price = price;
+            Size = size;
+            OperatingSystem = operatingSystem;
+            CameraResolution = cameraResolution;
+        }
+    }
+
+    class Animal : Product
+    {
+        public string Type { get; private set;}
+
+        public Animal(string name, int price, string manufacturer, string type)
+        {
+            Name = name;
+            Price = price;
+            Manufacturer = manufacturer;
+            Type = type;
         }
     }
 
@@ -28,7 +118,7 @@ namespace Shop
     {
         static void Main()
         {
-
+            Console.WriteLine("(owo)");
         }
     }
 }
