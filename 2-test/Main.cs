415594ea20b7bb4d40196d21e4661542f91202a2
StepanKobrii18321832 <esestepan2000@gmail.com>
2020-06-10 11:57:46 +0300

    new-test-2

--- 2-test/Main.cs
+++ 2-test/Main.cs
@@ -3,6 +3,27 @@ using System;
 namespace Shop 
 {
 
+    class User {
+        public string Name {get; private set;}
+        public int Age {get; private set;}
+        public string Phone_number {get; private set;}
+        public double Balance {get; private set;}
+        public double Spend {get; private set;}
+
+        public User(string Name, int Age)
+        {
+            //constructor
+        }
+        public User(string Name, int Age, string Phone_number)
+        {
+            //constructor
+        }
+        public User(string Name, int Age, string Phone_number, double Balance)
+        {
+            //constructor
+        }
+    }
+
     class Programm 
     {
         static void Main()
